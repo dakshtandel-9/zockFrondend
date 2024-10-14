8153b21583e2887952faf7b3fee0efc3dac5a54f
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2024-10-14 15:33:37 +0530

    sec commit

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',           // Bind to all network interfaces to make it accessible externally
-    port: process.env.PORT || 3000,  // Use the environment's port if available, otherwise fallback to 3000
+    port: 3000,  // Use the environment's port if available, otherwise fallback to 3000
     strictPort: true,          // Prevent Vite from switching ports if the specified one is busy
   },
   preview: {
     host: '0.0.0.0',           // Also bind for preview mode
-    port: process.env.PORT || 3000,
+    port: 3000,
   }
 });
