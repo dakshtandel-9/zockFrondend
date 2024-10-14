40e6bff6ccc86c839df9cbe3520787b95a8e0927
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2024-10-14 17:07:11 +0530

    4 commit-add

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './HeroSection.css'; // Importing the CSS file
 import TypingEffect from './TypingEffect'; // Importing the TypingEffect component
 import { useMediaQuery } from 'react-responsive';
+import Button from '@mui/material/Button';
 
 function HeroSection() {
     const phrases = [
@@ -15,22 +16,27 @@ function HeroSection() {
     const isMobile = useMediaQuery({ maxWidth: 768 });
 
     return (
-        <div style={{ background: "radial-gradient(circle, #296ceb, #2556d9)", paddingTop: "100px", height: "100vh" }}>
+        <div style={{ background: "radial-gradient(circle, #296ceb, #2556d9)", paddingTop: "100px", height: "100vh", textAlign: "center" }}>
             <div className='hero-container'>
                 <div className='left-part'>
-                    <img src="https://test.sknd.in/wp-content/uploads/2024/10/logo-footer.png" alt="Logo" />
+                    <img className='floating-img' src="https://test.sknd.in/wp-content/uploads/2024/10/logo-footer.png" alt="Logo" />
                 </div>
                 <div className='right-part'>
-                    {isMobile ? (<h1>
+                    {isMobile ? (<h1 style={{ color: "white" }}>
                         An Agency That Specialises In <br />
                         <TypingEffect phrases={phrases} />
-                    </h1>) : (<h1 style={{ fontSize: "3rem" }}>
+                    </h1>) : (<h1 style={{ fontSize: "3rem", color: "white" }}>
                         An Agency That<br /> Specialises In <br />
                         <TypingEffect phrases={phrases} />
                     </h1>)}
 
                 </div>
             </div >
+            <Button variant="contained" style={{ backgroundColor: "#ff3a47", width: "200px", borderRadius: "20px", height: "50px" }}>Hire Us</Button>
+            <div>
+                <img src="https://test.sknd.in/wp-content/uploads/2024/10/2-1.png" alt="" className='bg-img hover-image floating-img ' />
+                <img src="https://test.sknd.in/wp-content/uploads/2024/10/1-1.png" alt="" className='bg-img-2 hover-image-2 floating-img' />
+            </div>
         </div>
     );
 }
